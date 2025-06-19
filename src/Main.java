6dd4b37093980d01d9c3c3a9fa2dbfe2a2d2dbf2
numme <nummer14@naver.com>
2025-06-19 14:41:46 +0900

    First Commit

---
+++ src/Main.java
@@ -0,0 +1,33 @@
+class Box<T> {
+    private T content;
+
+    public Box(T content) {
+        this.content = content;
+    }
+
+    public T getContent() {
+        return content;
+    }
+
+    public void setContent(T content) {
+        this.content = content;
+    }
+
+    public <E> void printContent(E extraInfo) {
+        System.out.println("Content: " + content + ", Extra: " + extraInfo);
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        Box<String> stringBox = new Box<>("나는 커서 훌륭한 박스가 될거야!");
+
+        System.out.println(stringBox.getContent());
+
+        stringBox.setContent("실패!");
+
+        System.out.println(stringBox.getContent());
+
+        stringBox.printContent(123);
+    }
+}
