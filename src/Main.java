4dfa861b7799e6ce8738f2670b62f995c382c950
numme <nummer14@naver.com>
2025-06-20 09:24:19 +0900

    Fifth Commit

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,25 @@
-import java.util.Arrays;
-import java.util.List;
-
 public class Main {
-    public static void printElements(List<?> list) {
-        for (Object o : list) {
-            System.out.println(o);
+    private String data = "Outer data";
+
+    class Inner {
+        void printData() {
+            System.out.println(data);
         }
     }
-
+    public Inner createInner() {     // 반환타입이 Inner 여서
+        return new Inner();          // return 값으로 new Inner(); 가능
+    }                                // 즉, 메소드 호출로 Inner 클래스 인스턴스를 만드는 메소드
     public static void main(String[] args) {
-        List<Integer> numbers = Arrays.asList(1, 2, 3);
-        List<String> words = Arrays.asList("A", "B", "C");
+        Main outer = new Main();
 
-        printElements(numbers);
-        printElements(words);
+        Main.Inner inner = outer.createInner(); // Inner 클래스 인스턴스를 만드는 메소드
+        // outer.new Inner(); 로 대체 가능
+        inner.printData();
+        outer = null;                           // outer 메모리를 날려도
+        inner.printData();                      // 실행 됨
+        inner = null;                           // inner 메모리 날리면
+//      inner.printData();                      // 실행 안됨
+        System.gc();                            // 가비지 컬렉터 호출 요청
+        System.out.println("Outer와 Inner는 더 이상 사용되지 않음");
     }
 }
