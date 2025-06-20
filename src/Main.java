8a5fcc31f25d32a318e2132f8557056c33e8ea32
numme <nummer14@naver.com>
2025-06-20 09:39:14 +0900

    Sixth Commit

--- src/Main.java
+++ src/Main.java
@@ -1,25 +1,15 @@
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
-    private String data = "Outer data";
-
-    class Inner {
-        void printData() {
-            System.out.println(data);
-        }
-    }
-    public Inner createInner() {     // 반환타입이 Inner 여서
-        return new Inner();          // return 값으로 new Inner(); 가능
-    }                                // 즉, 메소드 호출로 Inner 클래스 인스턴스를 만드는 메소드
     public static void main(String[] args) {
-        Main outer = new Main();
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
 
-        Main.Inner inner = outer.createInner(); // Inner 클래스 인스턴스를 만드는 메소드
-        // outer.new Inner(); 로 대체 가능
-        inner.printData();
-        outer = null;                           // outer 메모리를 날려도
-        inner.printData();                      // 실행 됨
-        inner = null;                           // inner 메모리 날리면
-//      inner.printData();                      // 실행 안됨
-        System.gc();                            // 가비지 컬렉터 호출 요청
-        System.out.println("Outer와 Inner는 더 이상 사용되지 않음");
+        int result = numbers.stream()
+                .filter((n) -> n % 2 == 0)
+                .mapToInt(Integer::intValue)
+                .sum();
+
+        System.out.println(result);
     }
 }
